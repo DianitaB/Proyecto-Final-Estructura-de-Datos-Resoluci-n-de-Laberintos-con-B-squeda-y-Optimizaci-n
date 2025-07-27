88971f1d5eca9fb6d8923afe75bda681a0f9fccb
Erika-colla <collaguazobelen8@gmail.com>
2025-07-27 03:33:11 -0500

    feat(007): upd class Cell

--- src/main/java/models/Cell.java
+++ src/main/java/models/Cell.java
@@ -1,6 +1,41 @@
 package models;
+
 import java.util.Objects;
 
 public class Cell {
+    public int row;
+    public int col;
 
+    public  Cell(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell celda = (Cell) o;
+        return row == celda.row && col == celda.col;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(row, col);
+    }
+
+    @Override
+    public String toString() {
+        return "Celda{" +
+                row +
+                "," + col +
+                '}';
+    }
 }
